58a74b15dc8021e073fde3689ab4201f4fcd5d27
rightguydaniel <rightguydaniel@gmailcom>
2022-07-01 21:20:10 -0700

    hey

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
  * Description: Get a random number and check its last digit, compare it with 5
- *
  * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
-
+	
 	while (c <= 'z')
 	{
-		putchar(c);
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
 		c++;
 	}
 	putchar('\n');
